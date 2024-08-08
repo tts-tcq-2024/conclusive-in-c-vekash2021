dddf29f6f5af81612ee3369867223a04c2bee60f
vekash2021 <106808639+vekash2021@users.noreply.github.com>
2024-08-08 20:54:04 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -2,35 +2,33 @@
 #include <stdio.h>
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-  if(value < lowerLimit) {
-    return TOO_LOW;
-  }
-  if(value > upperLimit) {
-    return TOO_HIGH;
-  }
-  return NORMAL;
+    return (value < lowerLimit) ? TOO_LOW : (value > upperLimit) ? TOO_HIGH : NORMAL;
 }
 
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
-  int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
-      break;
-  }
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+
+struct CoolingLimits {
+    int lowerLimit;
+    int upperLimit;
+};
+
+CoolingLimits getCoolingLimits(CoolingType coolingType) {
+    switch(coolingType) {
+        case PASSIVE_COOLING:
+            return {0, 35};
+        case HI_ACTIVE_COOLING:
+            return {0, 45};
+        case MED_ACTIVE_COOLING:
+            return {0, 40};
+        default:
+            return {0, 0}; // Assuming there's a default behavior
+    }
 }
+
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
+    CoolingLimits limits = getCoolingLimits(coolingType);
+    return inferBreach(temperatureInC, limits.lowerLimit, limits.upperLimit);
+}
+
 
 void checkAndAlert(
     AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
